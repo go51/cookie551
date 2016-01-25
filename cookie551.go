31e00a7b65c76d2add2135ea1268a9585d1dc712
pubappbiz <ef+repository20160108@pubapp.biz>
2016-01-25 17:05:48 +0900

    refs #3 Cookie を登録するメソッドを実装する

--- cookie551.go
+++ cookie551.go
@@ -1,6 +1,9 @@
 package cookie551
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 type Cookie struct {
 	w http.ResponseWriter
@@ -14,4 +17,23 @@ func New(w http.ResponseWriter, r *http.Request) *Cookie {
 	}
 
 	return &cookie
+}
+
+func (c *Cookie) Set(name, value string, second time.Duration) {
+
+	cookie := http.Cookie{
+		Name:  name,
+		value: value,
+		Raw:   value,
+	}
+
+	if second == 0 {
+		cookie.MaxAge = 0
+	} else {
+		expire := time.Now().Add(second * time.Second)
+		cookie.Expires = expire
+	}
+
+	http.SetCookie(c.w, &cookie)
+
 }
